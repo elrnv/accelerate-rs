092e228573b97b7d1715e802b591ac0e9876c7e4
Egor Larionov <egor.larionov@gmail.com>
2022-03-20 09:23:29 -0700

    Cleanup C API testing scripts

--- accelerate-sys/capi/build.sh
+++ accelerate-sys/capi/build.sh
@@ -5,6 +5,6 @@
 mkdir -p build
 pushd build
 
-clang -c -Wall -Wno-nullability-completeness -o lib.o -I ../src ../src/api.c && ar rc ../libaccelerate_sparse.a lib.o
+clang -c -Wall -Wno-nullability-completeness -o lib.o -I ../src ../src/api.c && ar rc libaccelerate_sparse.a lib.o
 
 popd
--- accelerate-sys/capi/test/build.sh
+++ accelerate-sys/capi/test/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- clang++ -std=c++17 -Wall -Wno-nullability-completeness main.cpp -I../src ../libaccelerate_sparse.a -framework Accelerate
+ clang++ -std=c++17 -Wall -Wno-nullability-completeness main.cpp -I../src ../build/libaccelerate_sparse.a -framework Accelerate
